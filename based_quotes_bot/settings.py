4cad7b3941c093f86e2417884dc0c620a551804e
tetelevm <tetelev99@mail.ru>
2024-08-20 22:37:26 +0300

    Small changes in launch version option

--- based_quotes_bot/settings.py
+++ based_quotes_bot/settings.py
@@ -34,16 +34,16 @@ def _read_env():
     _ENVS = dict(line.split("=") for line in data)
 
 
-def _env(arg: str):
+def _env(arg: str, default=None):
     if _ENVS is None:
         _read_env()
-    return _ENVS[arg]
+    return _ENVS.get(arg, default)
 
 
-_ENVS_VERSION = ""
+_LAUNCH_VERSION = _env("LAUNCH_VERSION", "PRODUCTION")
 
 QUOTES_PATH = Path(__file__).parent / "quotes.txt"
 SENDING_TIME = datetime.timedelta(hours=5, minutes=0, seconds=0)  # UTC
 
-TOKEN = _env("TOKEN" + _ENVS_VERSION)
-CHAT_LINK = _env("CHAT_LINK" + _ENVS_VERSION)
+TOKEN = _env("TOKEN" + "_" + _LAUNCH_VERSION)
+CHAT_LINK = _env("CHAT_LINK" + "_" + _LAUNCH_VERSION)
